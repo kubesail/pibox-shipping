1211d483e0afdf0d68ec451327472881fe90d7a7
Dan Pastusek <dan@hexial.com>
2021-12-12 23:33:21 -0700

    fix line endings

--- bin/print_pdf.sh
+++ bin/print_pdf.sh
@@ -1,3 +1,8 @@
-gs -sDEVICE=bmpmono -r200 -f -sOutputfile=label.png label.pdf
+#!/bin/bash
+gs -sDEVICE=bmpmono -r200 -f -o label.png label.pdf
 gm convert label.png -crop 800x1198+450+80 label.png
-node raster-to-tspl-js/print.js label.png
+
+convert label.png -fill white -stroke white -draw "rectangle 620,32 775,84" label.png
+convert label.png +antialias -font DejaVu-Sans-Bold -pointsize 14 -annotate +620+32 "PiBox" label.png
+
+# node raster-to-tspl-js/print.js label.png
