59512e5713fdf709dd1281f5b9e6d2ea15987b02
Dan Pastusek <dan@hexial.com>
2021-12-12 23:17:31 -0700

    fix output file on gs

--- bin/print_pdf.sh
+++ bin/print_pdf.sh
@@ -1,3 +1,3 @@
-gs -sDEVICE=bmpmono -r200 -f -o label.png label.pdf
+gs -sDEVICE=bmpmono -r200 -f -sOutputfile=label.png label.pdf
 gm convert label.png -crop 800x1198+450+80 label.png
 node raster-to-tspl-js/print.js label.png
